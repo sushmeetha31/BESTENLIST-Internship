6ad19644cf549d32250fe4164e561f06d7087ce4
SUSHMEETHA J <74900821+sushmeetha31@users.noreply.github.com>
2020-11-24 16:57:50 +0530

    Add files via upload

---
+++ Day 2 task.py
@@ -0,0 +1,24 @@
+#Create three variables (a,b,c) to same value of any integer & do the following a
+#a) Divide a by 1 b) Multiply b by 50 #c) Add c value by 60
+a=10
+b=12
+c=15
+a=a/10
+b=b*50
+c=c+60
+print(a)
+print(b)
+print(c)
+
+#Create a string variable of 5 characters and replace the 3rd character with G
+s="watch"
+newstring=s.replace('t','G')
+print(newstring)
+
+#Create two values (a,b) of int,float data type &convert vice versa
+a=5
+b=5.0
+s=float(a)
+t=int(b)
+print(s)
+print(t)
